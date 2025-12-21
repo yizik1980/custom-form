f1af8b2ed1a3f1bcb1c5f1c048282d994b9c220e
Yizik <syizikm@gmail.com>
2025-12-21 10:40:54 +0200

    toaster

---
+++ custom-form/src/shared/Toast/Toast.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Toast = ({ message, type, onClose }) => {
+  return (
+    <div className={`toast toast-${type}`}>
+      <span>{message}</span>
+      <button className="toast-close" onClick={onClose}>×</button>
+    </div>
+  );
+};
+
+export default Toast;
